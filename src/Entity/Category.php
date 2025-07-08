f86b997769b23db5391977956b3c6c08401df16c
dsanchez <dsanchez@campus-eni.fr>
2025-07-08 11:22:39 +0200

    Mise en place de la relation entre Category et Course

--- src/Entity/Category.php
+++ src/Entity/Category.php
@@ -3,6 +3,8 @@
 namespace App\Entity;
 
 use App\Repository\CategoryRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 use Symfony\Component\Validator\Constraints as Assert;
@@ -27,9 +29,16 @@ class Category
     #[ORM\Column]
     private ?\DateTimeImmutable $dateCreated = null;
 
+    /**
+     * @var Collection<int, Course>
+     */
+    #[ORM\OneToMany(targetEntity: Course::class, mappedBy: 'category')]
+    private Collection $courses;
+
 
     public function __construct() {
         $this->dateCreated = new \DateTimeImmutable();
+        $this->courses = new ArrayCollection();
     }
 
     public function getId(): ?int
@@ -69,6 +78,36 @@ class Category
     public function setDateCreated(\DateTimeImmutable $dateCreated): static
     {
         $this->dateCreated = $dateCreated;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Course>
+     */
+    public function getCourses(): Collection
+    {
+        return $this->courses;
+    }
+
+    public function addCourse(Course $course): static
+    {
+        if (!$this->courses->contains($course)) {
+            $this->courses->add($course);
+            $course->setCategory($this);
+        }
+
+        return $this;
+    }
+
+    public function removeCourse(Course $course): static
+    {
+        if ($this->courses->removeElement($course)) {
+            // set the owning side to null (unless already changed)
+            if ($course->getCategory() === $this) {
+                $course->setCategory(null);
+            }
+        }
 
         return $this;
     }
